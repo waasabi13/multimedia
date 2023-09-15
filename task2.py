a2458fb856f4115a20b982776bc0e1eafcee00bf
kirill <kirilltitoff@mail.ru>
2023-09-15 19:59:55 +0300

    успешно сделано отображение видео в окне

--- task2.py
+++ task2.py
@@ -1,11 +1,13 @@
 import cv2
 
 img1 = cv2.imread(r'./source/meow1.jpeg',cv2.IMREAD_GRAYSCALE) #ЧБ
-img2 = cv2.imread(r'./source/meow2.png',cv2.IMREAD_UNCHANGED) #без изменений
-img3 = cv2.imread(r'./source/meow3.bmp',cv2.IMREAD_ANYDEPTH) # с учетом  глубины цвета
-cv2.namedWindow('test1', cv2.WINDOW_NORMAL)
-cv2.namedWindow('test2', cv2.WINDOW_NORMAL)
-cv2.namedWindow('test3', cv2.WINDOW_NORMAL)
+img2 = cv2.imread(r'./source/meow2.png',cv2.IMREAD_UNCHANGED)
+img3 = cv2.imread(r'./source/meow3.bmp',cv2.IMREAD_ANYDEPTH) #глубина цвета
+
+cv2.namedWindow('test1', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('test2', cv2.WINDOW_FULLSCREEN)
+cv2.namedWindow('test3', cv2.WINDOW_FREERATIO)
+
 cv2.imshow('test1',img1)
 cv2.imshow('test2', img2)
 cv2.imshow('test3', img3)
---
+++ task3.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture(r'./source/meow.mp4', cv2.CAP_ANY)
+
+new_width = 640
+new_height = 480
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        exit()
+    frame = cv2.resize(frame, (new_width, new_height))
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('meow video', gray_frame)
+    if cv2.waitKey(1) & 0xFF == 27:
+        exit()
