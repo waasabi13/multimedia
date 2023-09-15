1108ec0655092c12a77d45dcd489404483179956
kirill <kirilltitoff@mail.ru>
2023-09-15 20:24:02 +0300

    успешно сделана запись в файл

--- task3.py
+++ task3.py
@@ -6,11 +6,15 @@ new_width = 640
 new_height = 480
 
 while True:
+    # Читаем следующий кадр из видеопотока.
     ret, frame = cap.read()
     if not ret:
         exit()
-    frame = cv2.resize(frame, (new_width, new_height))
+    # Изменяем размер кадра на новые размеры (640x480).
+    frame = cv2.resize(frame, (new_width, new_height))  
+    # Преобразуем кадр в оттенки серого (градации серого).
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('meow video', gray_frame)
+
     if cv2.waitKey(1) & 0xFF == 27:
         exit()
---
+++ task4.py
@@ -0,0 +1,21 @@
+import cv2
+
+def readIPWriteTOFile():
+    video = cv2.VideoCapture(r'./source/meow.mp4', cv2.CAP_ANY)
+
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter("./output/output_meow.mp4", fourcc, 25, (w, h))
+    # while (True):
+    #     ok, vid = video.read()
+    #     cv2.imshow('Video', vid)
+    #     video_writer.write(vid)
+    #
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    video.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+readIPWriteTOFile()
